0673b7ec35d838399f6a56547adbfd14be3a7d30
German <geras210e@gmail.com>
2022-10-03 22:05:07 +0300

    Add repositories

---
+++ Repositories/OrderRepository.cs
@@ -0,0 +1,68 @@
+﻿using KitchenApi.Context;
+using KitchenApi.Interfaces;
+using KitchenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace KitchenApi.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly IKitchenContext _context;
+        private readonly ILogger<OrderRepository> _logger;
+        public OrderRepository(IKitchenContext context, ILogger<OrderRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<string?> AddOrderAsync(Order order, CancellationToken cancellationToken)
+        {
+            await _context.Orders.InsertOneAsync(order, cancellationToken);
+            _logger.LogInformation(order.Id is not null
+                ? $"order with id {order.Id} was created"
+                : $"failed to create order");
+            return order.Id;
+        }
+
+        public async Task<bool> DeleteOrderAsync(string id, CancellationToken cancellationToken)
+        {
+            var result = await _context.Orders.DeleteOneAsync(o => o.Id == id, cancellationToken);
+            var isDeletedSuccessfully = result.IsAcknowledged && result.DeletedCount > 0;
+            _logger.LogInformation(isDeletedSuccessfully
+                ? $"Order with id: {id} was successfuly deleted"
+                : $"Failed to delete order with id:{id}");
+            return isDeletedSuccessfully;
+        }
+
+        public async Task<IReadOnlyCollection<Order?>> GetAllOrdersAsync(CancellationToken cancellationToken)
+        {
+            var orders = await _context.Orders.Find(o => true).ToListAsync();
+            _logger.LogInformation(orders.Any()
+                ? $"all orders were retrieved"
+                : $"orders are't exist");
+            return orders;
+        }
+
+        public async Task<Order?> GetOrderAsync(string id, CancellationToken cancellationToken)
+        {
+            var order = await _context.Orders.Find(o => o.Id == id).SingleOrDefaultAsync();
+            _logger.LogInformation(order is not null
+                ? $"order with id:{id} was retrieved"
+                : $"order with id:{id} was't retrieved");
+            return order;
+        }
+
+        public async Task<bool> UpdateOrderAsync(Order order, CancellationToken cancellationToken)
+        {
+            var result = await _context.Orders.ReplaceOneAsync(o => o.Id == order.Id, order);
+            var isUpdatedSuccessfuly = result.IsAcknowledged && result.ModifiedCount > 0;
+
+            _logger.LogInformation(isUpdatedSuccessfuly
+                ? $"Order with id: {order.Id} was successfuly updated"
+                : $"Failed to update order with id:{order.Id}");
+            return isUpdatedSuccessfuly;
+        }
+    }
+}
--- Repositories/StatusRepository.cs
+++ Repositories/StatusRepository.cs
@@ -19,29 +19,36 @@ namespace KitchenApi.Repositories
         {
             await _context.Statuses.InsertOneAsync(status, cancellationToken);
 
-            _logger.LogInformation(status.Id is not null ?
-                $"status '{status.Title}' was created." :
-                $"status '{status.Title}' was not created.");
+            _logger.LogInformation(status.Id is not null 
+                ? $"status '{status.Title}' was created." 
+                : $"status '{status.Title}' was not created.");
             return status.Id;
         }
 
         public async Task<IReadOnlyCollection<Status>> GetAllStatusesAsync(CancellationToken cancellationToken)
         {
-            var statuses = await _context.Statuses.Find(s => true).ToListAsync();
+            var statuses = await _context.Statuses.Find(s => true).ToListAsync(cancellationToken);
+            _logger.LogInformation(statuses.Any()
+               ? $"all statuses were retrieved"
+               : $"all statuses were not retrieved");
             return statuses;
         }
 
-        public async Task<Status> GetStatusAsync(string id, CancellationToken cancellationToken)
+        public async Task<Status?> GetStatusAsync(string id, CancellationToken cancellationToken)
         {
-            var status = await _context.Statuses.Find(s => s.Id.Equals(id)).SingleOrDefaultAsync();
+            var status = await _context.Statuses.Find(s => s.Id.Equals(id)).SingleOrDefaultAsync(cancellationToken);
+            _logger.LogInformation(status is not null
+               ? $"status with {id} was retrieved"
+               : $"status with {id} not retrieved");
             return status;
         }
 
         public async Task<bool> IsStatusExistAsync(string title, CancellationToken cancellationToken)
         {
             var isStatusExist = await _context.Statuses.Find(s => s.Title == title).AnyAsync(cancellationToken);
-            _logger.LogInformation(isStatusExist ? $"status with title '{title}' is exist" :
-                                                  $"status with title '{title}' is not exist");
+            _logger.LogInformation(isStatusExist 
+                ? $"status with title '{title}' is exist" 
+                : $"status with title '{title}' is not exist");
             return isStatusExist;
         }
     }
