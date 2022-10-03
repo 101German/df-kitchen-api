33a9a0b01eb8fe0825ad072fc00335c2fe332563
German <geras210e@gmail.com>
2022-10-03 22:04:45 +0300

    Add interfaces

--- Interfaces/IStatusRepository.cs
+++ Interfaces/IStatusRepository.cs
@@ -4,7 +4,7 @@ namespace KitchenApi.Interfaces
 {
     public interface IStatusRepository
     {
-        Task<Status> GetStatusAsync(string id, CancellationToken cancellationToken);
+        Task<Status?> GetStatusAsync(string id, CancellationToken cancellationToken);
         Task<IReadOnlyCollection<Status>> GetAllStatusesAsync(CancellationToken cancellationToken);
         Task<string?> AddStatusAsync(Status status,CancellationToken cancellationToken);
         Task<bool> IsStatusExistAsync(string title, CancellationToken cancellationToken);
--- Interfaces/IStatusService.cs
+++ Interfaces/IStatusService.cs
@@ -1,12 +1,12 @@
-﻿using KitchenApi.Models;
+﻿using KitchenApi.DTO.Status;
 
 namespace KitchenApi.Interfaces
 {
     public interface IStatusService
     {
         Task<bool> AddStatusAsync(string statusTitle, CancellationToken cancellationToken);
-        Task<IReadOnlyCollection<Status>> GetAllStatusesAsync(CancellationToken cancellationToken);
-        Task<Status> GetStatusAsync(string id,CancellationToken cancellationToken);
+        Task<IReadOnlyCollection<StatusForReturn>> GetAllStatusesAsync(CancellationToken cancellationToken);
+        Task<StatusForReturn?> GetStatusAsync(string id,CancellationToken cancellationToken);
         Task<bool> IsStatusExistAsync(string title, CancellationToken cancellationToken);
     }
 }
